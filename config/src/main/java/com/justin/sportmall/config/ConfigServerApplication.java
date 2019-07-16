5609ca2faa24b3608136ed5a45563ec726a6f912
chenjianting <chenjianting@01zhuanche.com>
2019-07-16 19:11:31 +0800

    添加配置中心

--- config/src/main/java/com/justin/sportmall/config/ConfigServerApplication.java
+++ config/src/main/java/com/justin/sportmall/config/ConfigServerApplication.java
@@ -1,9 +1,16 @@
-package com.example.configserver;
+package com.justin.sportmall.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 配置中心
+ * @author chenjianting
+ * @data 2019/7/16 16:30
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
