2621707e787b14cba80d519b58dfb3549073586c
chenjianting <chenjianting@01zhuanche.com>
2019-07-16 14:57:11 +0800

    初始化

--- eureka/src/main/java/com/justin/sportmall/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/justin/sportmall/eureka/EurekaApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * eureka-server-starter
+ * @author chenjianting
+ * @date 2019/7/16 12:00
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
